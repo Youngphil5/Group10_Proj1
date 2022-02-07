59a1938d6a85fb77b15cc2d98a13530b143be354
ikasim <54857733+Isabel-Kasim@users.noreply.github.com>
2022-02-06 16:42:23 -0800

    Added Food.java and Meal.java, both needed for Retrofit to consume API information. Also Added SearchFood.java to act as the back end.

--- app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/models/Food.java
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/models/Food.java
@@ -1,2 +1,23 @@
-package com.daclink.drew.sp22.cst438_project01_starter.models;public class Food {
+package com.daclink.drew.sp22.cst438_project01_starter.models;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class Food {
+
+    @SerializedName("meals")
+    @Expose
+    private List<Meal> meals = null;
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+
 }
