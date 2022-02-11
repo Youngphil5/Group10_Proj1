1681ab5745b8ba61d30790c442ef595095ad19e7
ikasim <54857733+Isabel-Kasim@users.noreply.github.com>
2022-02-10 19:09:03 -0800

    added some changes to ApiTest.java. Finished up MealSearchResultsAdapter.java.

--- app/src/androidTest/java/com/daclink/drew/sp22/cst438_project01_starter/ApiTest.java
+++ app/src/androidTest/java/com/daclink/drew/sp22/cst438_project01_starter/ApiTest.java
@@ -1,4 +1,23 @@
 package com.daclink.drew.sp22.cst438_project01_starter;
 
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.daclink.drew.sp22.cst438_project01_starter.MealSearchResultsAdapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+
 public class ApiTest {
+    @Test
+    public void userInsertTest() {
+
+
+    }
 }
---
+++ app/src/main/java/com/daclink/drew/sp22/cst438_project01_starter/MealSearchResultsAdapter.java
@@ -0,0 +1,58 @@
+package com.daclink.drew.sp22.cst438_project01_starter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.daclink.drew.sp22.cst438_project01_starter.models.Meal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MealSearchResultsAdapter extends RecyclerView.Adapter<MealSearchResultsAdapter.MealSearchResultsHolder> {
+    private List<Meal> results = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public MealSearchResultsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_search, parent, false);
+        return new MealSearchResultsHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MealSearchResultsHolder holder, int position) {
+        Meal meal = results.get(position);
+
+        holder.mealTextView.setText(meal.getStrMeal());
+        //placeholder for if statements
+    }
+
+    @Override
+    public int getItemCount() {
+        return results.size();
+    }
+
+    public void setResults(List<Meal> results) {
+        this.results = results;
+        notifyDataSetChanged();
+    }
+
+
+    class MealSearchResultsHolder extends RecyclerView.ViewHolder {
+        private TextView mealTextView;
+
+        public MealSearchResultsHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mealTextView = itemView.findViewById(R.id.search_view);
+        }
+    }
+}
+
+
